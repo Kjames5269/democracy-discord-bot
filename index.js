bd16ed442d957a6fd1e606a9890a1c1f83c796dc
Kjames5269 <Kgrady1@email.arizona.edu>
2017-08-12 16:05:59 -0700

    Adding in votes with hardcoded values

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ client.on('ready', () => {
 	console.log('Ready Sirrrrrr.');
 });
 
+const demo = new RegExp('^Demo *');
+
 client.on('message', message => {
 	if(message.channel.name === 'constitution') {
 		if(!message.author.bot) {
@@ -13,7 +15,27 @@ client.on('message', message => {
 			message.delete();
 		}
 	}
-	if(message.content.subs
+	const temp = message.content.match(demo);
+	if(temp) {
+		handler(client,message);
+	}
 });
 
 client.login('MzQ0MzY2NDAxMjUyNzUzNDQw.DGrwUg.fp_ATYKP7Ph_C9m3531A3PYpwvs');
+
+
+function handler(cli, message) {
+	if(message.content.split(' ')[1] === 'vote') {
+		console.log('starting vote')
+		console.log(message.content.split(' ').length);
+		if(message.content.split(' ').length <= 2) {
+			message.reply('use vote with a message pls');
+			return;
+		} else {
+			message.reply('Started vote with ID: 1\n' + message.content.substring(message.content.indexOf('vote')+5) + ' vote 1 [yes] [no]');
+		}
+	}
+	if(message.content.split(' ')[1] === 'stuff') {
+		console.log('starting stuff');
+	}
+}
