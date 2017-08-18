d44bb2bb9ace68ef2a172b19569988c721beb5d2
Kjames5269 <Kgrady1@email.arizona.edu>
2017-08-17 20:47:28 -0700

    Added the database. We are off the ground boys.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const MessageHandler = require('./messageHandler.js');
+const Vote = require('./voteHandler.js');
 
 client.on('ready', () => {
 	console.log('Ready Sirrrrrr.');
@@ -26,14 +27,7 @@ client.login('MzQ0MzY2NDAxMjUyNzUzNDQw.DGrwUg.fp_ATYKP7Ph_C9m3531A3PYpwvs');
 
 function handler(cli, message) {
 	if(message.content.split(' ')[1] === 'vote') {
-		console.log('starting vote')
-		console.log(message.content.split(' ').length);
-		if(message.content.split(' ').length <= 2) {
-			message.reply('use vote with a message pls');
-			return;
-		} else {
-			message.reply('Started vote with ID: 1\n' + message.content.substring(message.content.indexOf('vote')+5) + ' vote 1 [yes] [no]');
-		}
+		Vote.handler(cli, message);
 	}
 	if(message.content.split(' ')[1] === 'stuff') {
 		console.log('starting stuff');
---
+++ src/voteHandler.js
@@ -0,0 +1,95 @@
+const db = require('./database.js');
+const Promise = require('bluebird');
+
+const errMsg = 'vote is used as follows: \nvote "message" to start a vote\nvote voteID [yes, no]';
+const VOTE_ID_S = 2;
+const VOTE_S = 3
+
+export function handler(cli, message) {
+  console.log('starting vote')
+  const voteArr = message.content.split(' ');
+
+  console.log(voteArr.length);
+  if(voteArr.length <= 2) {
+    message.reply(errMsg);
+    return;
+  }
+  else if (isNaN(voteArr[VOTE_ID_S])) {
+    db.getNewVoteID().then((doc, err) => {
+      //console.log(doc);
+      const voteID = doc.value.lastVoteID;
+      const voteMsg = message.content.substring(message.content.indexOf('vote')+5);
+      db.addNewVote(voteID, voteMsg).then((doc, err) => {
+        //console.log(doc);
+        message.reply('Started vote with ID: ' + voteID + '\n'
+        + voteMsg + '\nvote ' + voteID + ' [yes] [no]');
+      }).catch(e => {
+        message.reply('There was a problem starting the vote at this time');
+        console.log('Caught error during insertOne');
+        console.log(e);
+      });
+    }).catch(e => {
+      message.reply('There was a problem starting the vote at this time');
+      console.log('Caught error during getVoteId');
+      console.log(e);
+    });
+  }
+  else if (voteArr.length <= 4 ) {
+    if(voteArr[VOTE_S] === 'yes') {
+      preVote(voteArr[VOTE_ID_S], voteArr[VOTE_S], message);
+    }
+    else if(voteArr[VOTE_S] === 'no') {
+      preVote(voteArr[VOTE_ID_S], voteArr[VOTE_S], message);
+    }
+    else {
+      if(isNaN(voteArr[VOTE_ID_S])) {
+        message.reply(errMsg);
+      }
+      else {
+        postResults(parseInt(voteArr[VOTE_ID_S]),message);
+      }
+    }
+  }
+  else {
+    console.log(voteArr);
+    message.reply(errMsg);
+  }
+};
+
+function preVote(voteID, vote, message) {
+  if(isNaN(voteID)) {
+    message.reply('vote id [yes],[no]. help for more');
+  }
+  else {
+    db.voteOn(parseInt(voteID), message.author.id, vote).then((doc, err) => {
+      //console.log(doc);
+      //console.log(err);
+      postResults(parseInt(voteID),message);
+    });
+  }
+}
+
+function postResults(voteID, message) {
+  db.getResults(voteID).then((doc, err) => {
+    //console.log(doc);
+    if(doc === null) {
+      //  invalid vote number
+      message.reply('No votes found with ID ' + voteID);
+      return
+    }
+    const users = doc.users;
+    var yes = 0;
+    var no = 0;
+
+    for(var i = 0; i < users.length; i++) {
+      if(users[i].vote === 'yes') {
+        yes += 1;
+      }
+      else if( users[i].vote === 'no') {
+        no += 1;
+      }
+    }
+
+    message.reply(doc.message + ': ' + yes + ' for, ' + no + ' against');
+  });
+}
